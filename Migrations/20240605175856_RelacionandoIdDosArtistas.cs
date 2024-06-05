cdd9fa522851d4cc9ea7ea4eed200630f8e13cf3
GuilhermeSerafim <guilerstudies@gmail.com>
2024-06-05 15:04:56 -0300

    Relacionando ArtistaID com id da musica com migration

---
+++ Migrations/20240605175856_RelacionandoIdDosArtistas.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound5.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionandoIdDosArtistas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 2 WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 3 WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 4 WHERE Id = 4");
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 5 WHERE Id = 5");
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM MUSICAS");
+
+        }
+    }
+}
