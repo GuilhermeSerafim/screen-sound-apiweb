f0d78cb1c3099ad2738034720c636ef080f4bd22
GuilhermeSerafim <guilerstudies@gmail.com>
2024-06-06 15:42:10 -0300

    Adicionando construtor vazio na classe artista para tratar o problea ILLink durante a compilação - desserialização

--- ScreenSound.Shared.Modelos/Modelos/Artista.cs
+++ ScreenSound.Shared.Modelos/Modelos/Artista.cs
@@ -15,6 +15,8 @@ public class Artista
         FotoPerfil = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
     }
 
+    public Artista(){}
+
     public string Nome { get; set; }
     public string FotoPerfil { get; set; }
     public string Bio { get; set; }
