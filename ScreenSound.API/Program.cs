fad25e16a60e9fbbaf033dca4b61d7651e45a7a1
GuilhermeSerafim <guilerstudies@gmail.com>
2024-06-11 11:25:03 -0300

    Instalando Swagger e configurando variaveis ambiente para inicialização da api

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -23,8 +23,13 @@ builder.Services.AddTransient<GenericDAL<Musica>>();  // Cria uma nova instância
 //  ReferenceHandler.IgnoreCycles: Define o ReferenceHandler como IgnoreCycles. Isso significa que, durante a serialização, se houver ciclos de referência
 //  (por exemplo, um objeto A referenciando um objeto B, que por sua vez referencia o objeto A), esses ciclos serão ignorados em vez de causar uma exceção.
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Documentação dinamica
+// Termos importantes:
+// Middleware: Componentes que processam requisições HTTP em um pipeline.
+// Pipeline de Middleware: Sequência de middleware que uma requisição percorre do início ao fim.
+builder.Services.AddEndpointsApiExplorer(); // adiciona serviços necessários para explorar e documentar os endpoints da API.
+builder.Services.AddSwaggerGen(); // adiciona os serviços necessários para gerar a documentação Swagger.
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>
     (options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
@@ -32,5 +37,8 @@ var app = builder.Build();
 
 app.AddEndpointsArtistas();
 app.AddEndpointsMusicas();
+
+app.UseSwagger(); // Configura o middleware para gerar e servir a especificação OpenAPI da API como um endpoint JSON.
+app.UseSwaggerUI();  // Configura o middleware para servir a interface de usuário do Swagger, permitindo a exploração e teste interativos da API no navegador.
 
 app.Run();
